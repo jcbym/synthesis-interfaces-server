8a79ebbff4786cbccc04cd39418ac60dff2aabb8
Justin Lubin <jlubi333@gmail.com>
2024-03-18 21:19:22 -0700

    Switch to https

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const https = require("http");
+const https = require("https");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Constants
